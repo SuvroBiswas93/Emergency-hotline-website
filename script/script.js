be76cf736031514b0353aba4c45761b02314d62a
Suvro Biswas <shuvo.249963@gmail.com>
2025-08-27 18:17:55 +0600

    implement Heart Count funtionalities

--- script/script.js
+++ script/script.js
@@ -1,2 +1,12 @@
 
-const heartIcons = document.getElementsByClassName('heart-icon')
+ const heartIcons = document.getElementsByClassName('heart-icon');
+ let heartCount = Number(document.getElementById('nav-heart-icon').innerText);
+
+ for (const heart of heartIcons) {
+   heart.addEventListener('click',function(){
+    heartCount++
+    document.getElementById('nav-heart-icon').innerText = heartCount;
+   })
+
+ }
+
