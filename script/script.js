4798188c40f24c5b9e5d7e640498fc1deae239bb
Suvro Biswas <shuvo.249963@gmail.com>
2025-08-27 19:32:11 +0600

    Add copy feature get hotline number feature and implement counting feature

--- script/script.js
+++ script/script.js
@@ -8,15 +8,45 @@ function getElement(id){
  * implentation of heart counting. when user Will click heart icon
  *  inside card it will increase count for nav bar heart icon counting 
  */
+
  const heartIcons = document.getElementsByClassName('heart-icon');
  let heartCount = Number(getElement('nav-heart-icon').innerText);
 
  for (const heart of heartIcons) {
    heart.addEventListener('click',function(){
         heartCount++;
-    document.getElementById('nav-heart-icon').innerText = Number(heartCount);
+    getElement('nav-heart-icon').innerText = Number(heartCount);
    })
  }
+
+ /**
+  * Implement card copy button feature. when user click copy button inside card 
+  * it will copy and and show the copy count in navbar
+  */
+
+ const copyBtn = document.getElementsByClassName('btn-copy');
+ let copyCount = Number(getElement("nav-btn").innerText);
+
+ for (const count of copyBtn) {
+    count.addEventListener("click",function(){
+        copyCount++
+        getElement("nav-btn").innerText = Number(copyCount)
+
+        const hotlineNumber = count.parentNode.parentNode.children[2].children[0].innerText
+
+        const setHotlineNumber = document.getElementsByClassName('hotline-number')
+      
+        
+        setHotlineNumber.innerText = hotlineNumber;
+
+        alert('Copied Hotline Number ' + hotlineNumber);
+
+        navigator.clipboard.writeText(hotlineNumber);
+     
+    })
+ }
+
+
 
 
 
